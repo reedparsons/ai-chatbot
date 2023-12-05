cc7e508ca436094559d468c8aff3bc930ead6660
reedparsons <nr.parsons@uleth.ca>
2023-12-05 16:30:36 -0700

    uipdated url

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = {
       '833345774603-3pondr11uqoj3rfhv9nh0m31hitqivlk.apps.googleusercontent.com',
     GOOGLE_CLIENT_SECRET: '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',
     GOOGLE_API_KEY: 'AIzaSyC0jcq6RW0WNuyE3jTmSx-b-4g7DUR9F7c',
-    NEXTAUTH_URL: 'http://ai-chatbot-nik-og.vercel.app/',
+    NEXTAUTH_URL: 'https://chat.nestech-ai.com',
     NEXTAUTH_SECRET: 'YEfFAYPE7HljOU4aXarP7gsc/FQJWJnH/XwnemuN7e0=',
     AUTH_GITHUB_ID: '1dc8455c3b242540fe13',
     AUTH_GITHUB_SECRET: '80684574f046e1c000522d5636b38692e686828f',
@@ -20,10 +20,13 @@ module.exports = {
       'AbPaASQgYmY5NWZlYWUtMWNiMC00OGQ0LWFlZWQtZDFkMzc5OGMwYmQwY2VhYzI3YWQ3ZTk3NDM0MjkzOWQ3MjAxNDExMzg0NGE=',
     KV_REST_API_READ_ONLY_TOKEN:
       'ArPaASQgYmY5NWZlYWUtMWNiMC00OGQ0LWFlZWQtZDFkMzc5OGMwYmQw8Sx-mckp0BD1rVZK85igteSVdWMWuRjEOE27m1ztoeg=',
-    KVVV_URL:"redis://default:a8cae0620151437dbe51a7cadcb6831a@immune-bison-48459.kv.vercel-storage.com:48459",
-    KVVV_REST_API_URL:"https://immune-bison-48459.kv.vercel-storage.com",
-    KVVV_REST_API_TOKEN:"Ab1LASQgYzkxNDEwMGQtZmNlNS00YmE2LThlNWMtMWZiNjQ5MzhkZWY0YThjYWUwNjIwMTUxNDM3ZGJlNTFhN2NhZGNiNjgzMWE=",
-    KVVV_REST_API_READ_ONLY_TOKEN:"Ar1LASQgYzkxNDEwMGQtZmNlNS00YmE2LThlNWMtMWZiNjQ5MzhkZWY0nchOZs_3eK2XKnGuHfDgpoEUueK-fgBq8r_78OepFXo=",
+    KVVV_URL:
+      'redis://default:a8cae0620151437dbe51a7cadcb6831a@immune-bison-48459.kv.vercel-storage.com:48459',
+    KVVV_REST_API_URL: 'https://immune-bison-48459.kv.vercel-storage.com',
+    KVVV_REST_API_TOKEN:
+      'Ab1LASQgYzkxNDEwMGQtZmNlNS00YmE2LThlNWMtMWZiNjQ5MzhkZWY0YThjYWUwNjIwMTUxNDM3ZGJlNTFhN2NhZGNiNjgzMWE=',
+    KVVV_REST_API_READ_ONLY_TOKEN:
+      'Ar1LASQgYzkxNDEwMGQtZmNlNS00YmE2LThlNWMtMWZiNjQ5MzhkZWY0nchOZs_3eK2XKnGuHfDgpoEUueK-fgBq8r_78OepFXo=',
     VERCEL: '6Bc5IcwNcgJLr5Ayo529wFgr'
   },
   images: {
